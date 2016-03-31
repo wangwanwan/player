8597cc21230974e5de349c36bda27b1116db6bf1
sdduser <sdduser@aclgcl-ubnt.(none)>
2016-03-31 18:59:15 +0800

    add feature

--- js/app.js
+++ js/app.js
@@ -2,6 +2,15 @@
 	var player = document.getElementById("player");
 	var play = document.getElementById("play");
 	var stop = document.getElementById("stop");
+  var volume = document.getElementById("volume");
+  var innerVolume = document.getElementById("innerVolume");
+  var timer = document.getElementById("timer");
+  var outProgress = document.getElementById("outProgress");
+  var innerProgress = document.getElementById("innerProgress");
+  var outVolume = document.getElementById("outVolume");
+  var innerVolume = document.getElementById("innerVolume");
+
+
 
 	/*document.onclick = function(event){
 		var target = event.target;
@@ -15,20 +24,105 @@
         	}
 	}*/
 
-	play.onclick = function(){
+	play.onclick = function () {
 		if (player.paused) {
-            	player.play(); //播放
-            	play.setAttribute("class", "fa fa-pause");
-        } else {
-           	 player.pause(); //暂停
-            	 play.setAttribute("class", "fa fa-play");
-        }
+      player.play(); //播放
+      play.setAttribute("class", "fa fa-pause");
+    } else {
+      player.pause(); //暂停
+      play.setAttribute("class", "fa fa-play");
+
+      }
 	}
         
-       stop.onclick = function(){
-       	 player.currentTime = 0; //停止播放
-        	// $innerBar.css('width', 0 + 'px');
-       }
+  stop.onclick = function () {
+    player.currentTime = 0; //停止播放
+      document.getElementById("innerProgress").style.width = 0;
+  }
+
+  volume.onclick = function () {
+    if (player.muted) {
+        player.muted = false;
+        volume.setAttribute("class","fa fa-volume-up");
+        innerVolume.style.width = 100+"%";
+      } else {
+        player.muted = true;
+        volume.setAttribute("class","fa fa-volume-off");
+        innerVolume.style.width = 0;
+
+      }
+
+
+  }
+  player.addEventListener("timeupdate",function () {
+
+    var time = player.currentTime.toFixed(1),
+    minutes = Math.floor((time / 60) % 60),
+    seconds = Math.floor(time % 60);
+
+    if (seconds < 10) {
+      seconds = '0' + seconds;
+    }
+    timer.innerHTML = minutes + ':' + seconds;
+
+    var width = outProgress.offsetWidth;
+      
+    if (player.duration) {
+      var per = (player.currentTime / player.duration).toFixed(3);
+        
+    } else {
+      per = 0;
+
+    }
+      
+      innerProgress.style.width = (width * per).toFixed(0) + 'px';
+      // alert(innerProgress.style.width);
+      // alert(width*per);
+
+      if (player.ended) { 
+        play.setAttribute("class", "fa fa-play");     
+      }
+   
+
+
+});
+
+  
+  outProgress.onclick = function (event) {
+    var width = event.target.offsetWidth;
+    var widthX = event.offsetX;
+    var duration = player.duration;
+
+    per = (widthX / width).toFixed(3);
+    player.currentTime = (duration * per).toFixed(0);
+    innerProgress.style.width = widthX + 'px';
+
+      // alert(width);
+
+
+    }
+
+    // $volumeControl
+    // .on('click', function(e) {
+    //   var w = $(this).width(),
+    //     x = e.offsetX;
+    //   window.vol = (x / w).toFixed(1);
+
+    //   player.volume = window.vol;
+    //   $volumeInner.css('width', x + 'px');
+    // });
+
+    outVolume.onclick = function (event) {
+      var width = event.target.offsetWidth;
+      var widthX = event.offsetX;
+    
+      player.volume = (widthX / width).toFixed(3);
+      innerVolume.style.width = widthX + "px";
+
+
+
+    }
+
   
 
 
