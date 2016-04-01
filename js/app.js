448e76d67887e82db6de45781f5d91708ffe0fc3
sdduser <sdduser@aclgcl-ubnt.(none)>
2016-04-01 18:44:07 +0800

    fixed some bug for example stop function

--- js/app.js
+++ js/app.js
@@ -10,9 +10,6 @@
   var outVolume = document.getElementById("outVolume");
   var innerVolume = document.getElementById("innerVolume");
 
-
-
-	
 	play.onclick = function () {
 		if (player.paused) {
       player.play(); //play
@@ -26,7 +23,9 @@
         
   stop.onclick = function () {
     player.currentTime = 0;
-      document.getElementById("innerProgress").style.width = 0;
+    innerProgress.style.width = 0;
+    player.pause();
+    play.setAttribute("class", "fa fa-play");
   }
 
   volume.onclick = function () {
@@ -34,13 +33,12 @@
         player.muted = false;
         volume.setAttribute("class","fa fa-volume-up");
         innerVolume.style.width = 100+"%";
-      } else {
-        player.muted = true;
-        volume.setAttribute("class","fa fa-volume-off");
-        innerVolume.style.width = 0;
+    } else {
+      player.muted = true;
+      volume.setAttribute("class","fa fa-volume-off");
+      innerVolume.style.width = 0;
 
       }
-
 
   }
   player.addEventListener("timeupdate",function () {
@@ -53,7 +51,6 @@
       seconds = '0' + seconds;
     }
     timer.innerHTML = minutes + ':' + seconds;
-
     var width = outProgress.offsetWidth;
       
     if (player.duration) {
@@ -64,49 +61,33 @@
 
     }
       
-      innerProgress.style.width = (width * per).toFixed(0) + 'px';
-      // alert(innerProgress.style.width);
-      // alert(width*per);
+    innerProgress.style.width = (width * per).toFixed(0) + 'px';
+    // alert(innerProgress.style.width);
+    // alert(width*per);
 
-      if (player.ended) { 
-        play.setAttribute("class", "fa fa-play");     
-      }
-   
-
+    if (player.ended) { 
+      play.setAttribute("class", "fa fa-play");     
+    }
 
 });
-
   
   outProgress.onclick = function (event) {
     var width = event.target.offsetWidth;
-    var widthX = event.offsetX;
+    var widthX = event.offsetX;//it isn't run on the simulator.
     var duration = player.duration;
 
     per = (widthX / width).toFixed(3);
     player.currentTime = (duration * per).toFixed(0);
-    innerProgress.style.width = widthX + 'px';
-
-      
-
-
-    }
-
-   
-
-    outVolume.onclick = function (event) {
-      var width = event.target.offsetWidth;
-      var widthX = event.offsetX;
-    
-      player.volume = (widthX / width).toFixed(3);
-      innerVolume.style.width = widthX + "px";
-
-
-
-    }
-
+    innerProgress.style.width = widthX + 'px';    
+  }
   
 
-
-
+  outVolume.onclick = function (event) {
+    var width = event.target.offsetWidth;
+    var widthX = event.offsetX;
+    
+    player.volume = (widthX / width).toFixed(3);
+    innerVolume.style.width = widthX + "px";
+  }
 
 })();
